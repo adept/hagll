8b08a412fb56d58bbb6ee747ab816bc2adceb0b9
Dmitry Astapov <dastapov@gmail.com>
2010-03-02 11:38:50 +0200

    Изменил монаду со State на RWS, чтобы отладочный лог был в правильном порядке

--- Scratch.hs
+++ Scratch.hs
@@ -2,7 +2,7 @@ module Scratch where
 
 import Data.Map as M
 import Data.Set as S
-import Control.Monad.State
+import Control.Monad.RWS
 import Data.Maybe
 import Debug.Trace
 
@@ -25,13 +25,14 @@ type E = Map Node (Set Node) -- parents
 
 data ParserState = PS { gee :: G, pee :: P, er::R, pe::P, curr_i :: Pos, curr_u :: Node, input::Input, parents :: E, yu::U } deriving Show
 mkPS inp = PS S.empty S.empty [] S.empty 0 node_0 inp M.empty M.empty
-type Parser a = State ParserState a
+type Parser a = RWS () String ParserState a
+tellLn s = tell $ s ++ "\n"
 
 data Result = Success | Failure deriving Show
 
 create :: Label -> Node -> Pos -> Parser Node
 create label u i = do
-  say "create"
+  tellLn "create"
   g <- gets gee
   p <- gets pee
   if not ((label, i) `S.member` g)
@@ -44,7 +45,7 @@ create label u i = do
 
 add :: Label -> Node -> Pos -> Parser ()
 add label u i = do
-  say $ "add " ++ show (label,u,i)
+  tellLn $ "add " ++ show (label,u,i)
   r <- gets er
   yu_ <- gets yu
   let u_i = maybe [] id $ M.lookup i yu_
@@ -53,7 +54,7 @@ add label u i = do
      else return ()
 
 pop u i = do
-  say $ "pop" ++ show (u,i)
+  tellLn $ "pop" ++ show (u,i)
   let (label, _) = u
   pts <- gets parents
   modify (\s -> s{pe = S.insert (u,i) (pe s)})
@@ -61,25 +62,23 @@ pop u i = do
     then forM_ (S.elems (pts!u)) $ \v -> add label v i
     else return ()
   
-say s = trace s (return ())
-
 l_666 = L 666 undefined
 node_0 = (l_666, (-1))
 parse = do
-  say "parse"
+  tellLn "parse"
   add l_s node_0 0
   goto l_0
   
-goto (L id f) = say ("goto " ++ show id) >> f 
+goto (L id f) = tellLn ("goto " ++ show id) >> f 
 
 l_0 = L 0 $ do
   r <- gets er
   c_i <- gets curr_i
   inp <- gets input
-  say "l_0"
+  tellLn "l_0"
   if not (Prelude.null r) 
     then do let (label, c_u, c_i) = head r
-            say $ "  l_0, R is " ++ show r
+            tellLn $ "  l_0, R is " ++ show r
             modify (\s -> s{er = tail r, curr_u = c_u, curr_i = c_i})
             goto label
             
@@ -88,23 +87,23 @@ l_0 = L 0 $ do
          else return Failure
               
 l_s = L 5 $ do
-  say "l_s"
+  tellLn "l_s"
   c_i <- gets curr_i
   c_u <- gets curr_u
   inp <- gets input
   if inp!!c_i == 'a'
-    then do say "l_s, case 1"
+    then do tellLn "l_s, case 1"
             add l_s0 c_u c_i
             add l_s1 c_u c_i
             goto l_0
     else if inp!!c_i == '$'
-            then do say "l_s, case 2"
+            then do tellLn "l_s, case 2"
                     add l_s1 c_u c_i
                     goto l_0
-            else say "l_s, case 3" >> goto l_0
+            else tellLn "l_s, case 3" >> goto l_0
                              
 l_s0 = L 50 $ do
-  say "l_s0"
+  tellLn "l_s0"
   c_i <- gets curr_i
   c_u <- gets curr_u
   inp <- gets input
@@ -115,7 +114,7 @@ l_s0 = L 50 $ do
      else goto l_0
           
 l_s0' = L 501 $ do
-  say "l_s0'"
+  tellLn "l_s0'"
   c_i <- gets curr_i
   c_u <- gets curr_u
   inp <- gets input
@@ -123,7 +122,7 @@ l_s0' = L 501 $ do
   goto l_0
   
 l_s1 = L 51 $ do
-  say "l_s1"
+  tellLn "l_s1"
   c_i <- gets curr_i
   c_u <- gets curr_u
   inp <- gets input
@@ -133,7 +132,7 @@ l_s1 = L 51 $ do
      else goto l_0
 
 l_s2 = L 52 $ do
-  say "l_s2"
+  tellLn "l_s2"
   c_i <- gets curr_i
   c_u <- gets curr_u
   inp <- gets input
@@ -142,4 +141,7 @@ l_s2 = L 52 $ do
             goto l_0
     else goto l_0
          
-main = print $ runState parse (mkPS "a$")
+main = do
+  let (ret, log) = evalRWS parse () (mkPS "a$")
+  print ret
+  putStrLn log
